383dc48d0d32dc6267d3e14ad923056d94a8daa9
peaceInayat <peace.inayat@gmail.com>
2017-07-24 15:49:00 +0530

    access keys social media

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,12 @@ module Ra
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
   end
 end

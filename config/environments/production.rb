0d4ddeea79b4aac903c30485fac691f8105ff307
peaceInayat <peace.inayat@gmail.com>
2017-08-20 15:01:25 +0530

    as

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config.assets.compile = true
+  config.action_mailer.default_url_options = { :host => 'agile-peak-28298.herokuapp.com' }
 
 end
